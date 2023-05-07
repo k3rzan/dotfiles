8fb042b09dabda14c1e39f833689bb3bcc472134
Beicker Zambrano <beicker998@gmail.com>
2023-05-06 20:31:13 -0500

    Add lazy loading to as many plugins as possible
    Add lazy loading to the plugins that were least needed in general such
    as language specific plugins like vim-godot and make them load only when
    needed or in specific filetypes.
    
    Change the lsp-zero config to adapt it
    to Lazy with lazy loading by copying pasting the code example suggested
    by the creators:
    https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/guides/lazy-loading-with-lazy-nvim.md

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -17,61 +17,121 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup(
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v2.x',
-		dependencies = {
-			-- LSP Support
-			{ 'neovim/nvim-lspconfig' }, -- Required
-			{
-				-- Optional
-				'williamboman/mason.nvim',
-				build = function()
-					pcall(vim.cmd, 'MasonUpdate')
-				end,
+
+		--Colors
+		{
+			'navarasu/onedark.nvim',
+			lazy = false,
+			priority = 1000,
+			config = function()
+				--colorscheme
+				require('onedark').setup({
+					style = 'dark',
+				})
+				require('onedark').load()
+			end
+		},
+
+		{ 'nvim-lua/plenary.nvim',           lazy = true },
+		{ 'jose-elias-alvarez/null-ls.nvim', lazy = true },
+		--IDE
+		{ 'vim-airline/vim-airline' },
+		{ 'vim-airline/vim-airline-themes' },
+		{ 'nvim-telescope/telescope.nvim',   tag = '0.1.1', lazy = true },
+		{ 'tpope/vim-surround',              lazy = true },
+		{ 'jiangmiao/auto-pairs' },
+		{
+			'scrooloose/nerdtree',
+		},
+		{ 'Yggdroot/indentLine' },
+		{ 'simrat39/rust-tools.nvim',       ft = "rs" },
+		{ 'nvim-treesitter/nvim-treesitter' },
+		{ 'virchau13/tree-sitter-astro',    ft = "astro" },
+		{ 'airblade/vim-gitgutter' },
+		{ 'alvan/vim-closetag',             ft = { "typescriptreact", "javascriptreact", "javascript", "jsx", "tsx" } },
+		{ 'christoomey/vim-tmux-navigator' },
+		{ 'lambdalisue/suda.vim',           lazy = true },
+		{ 'mhinz/vim-signify',              lazy = true },
+		{ 'ThePrimeagen/harpoon',           lazy = true },
+		--Godot
+		{ 'habamax/vim-godot',              ft = "gd" },
+		--Development
+		{
+			'MunifTanjim/prettier.nvim',
+			ft = { "javascript", "javascriptreact", "typescriptreact", "typescript", "jsx", "tsx",
+				"html", "css", "md" }
+		},
+		{ 'simrat39/rust-tools.nvim', ft = { "rs" } },
+		{
+			'VonHeikemen/lsp-zero.nvim',
+			branch = 'v2.x',
+			lazy = true,
+			config = function()
+				-- This is where you modify the settings for lsp-zero
+				-- Note: autocompletion settings will not take effect
+
+				require('lsp-zero.settings').preset({})
+			end
+		},
+
+		-- Autocompletion
+		{
+			'hrsh7th/nvim-cmp',
+			event = 'InsertEnter',
+			dependencies = {
+				{ 'L3MON4D3/LuaSnip' },
 			},
-			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
+			config = function()
+				-- Here is where you configure the autocompletion settings.
+				-- The arguments for .extend() have the same shape as `manage_nvim_cmp`:
+				-- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#manage_nvim_cmp
 
-			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },  -- Required
-			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
-			{ 'L3MON4D3/LuaSnip' },  -- Required
-			{ 'nvim-lua/plenary.nvim' },
-			{ 'jose-elias-alvarez/null-ls.nvim' },
-			{ 'hrsh7th/cmp-buffer' },
-			{ 'hrsh7th/cmp-path' },
-			--Colors
-			{ 'navarasu/onedark.nvim' },
-			--IDE
-			{ 'vim-airline/vim-airline' },
-			{ 'nvim-telescope/telescope.nvim',    tag = '0.1.1', },
-			{ 'tpope/vim-surround' },
-			{ 'vim-airline/vim-airline-themes' },
-			{ 'jiangmiao/auto-pairs' },
-			{ 'scrooloose/nerdtree' },
-			{ 'Yggdroot/indentLine' },
-			{ 'simrat39/rust-tools.nvim' },
-			{ 'nvim-treesitter/nvim-treesitter',  cmd = 'TSUpdate' },
-			{ 'virchau13/tree-sitter-astro' },
-			{ 'airblade/vim-gitgutter' },
-			{ 'alvan/vim-closetag' },
-			{ 'christoomey/vim-tmux-navigator' },
-			{ 'lambdalisue/suda.vim' },
-			{ 'preservim/nerdcommenter' },
-			{ 'mhinz/vim-signify' },
-			{ 'ThePrimeagen/harpoon' },
-			--Godot
-			{ 'habamax/vim-godot' },
-			--Development
-			{ 'MunifTanjim/prettier.nvim' },
-			{ 'simrat39/rust-tools.nvim' },
+				require('lsp-zero.cmp').extend()
 
+				-- And you can configure cmp even more, if you want to.
+				local cmp = require('cmp')
+				-- local cmp_action = require('lsp-zero.cmp').action()
 
+				--cmp.setup({
+				--		mapping = {
+				--			['<C-Space>'] = cmp.mapping.complete(),
+				--			['<C-f>'] = cmp_action.luasnip_jump_forward(),
+				--			['<C-b>'] = cmp_action.luasnip_jump_backward(),
+				--		}
+				--	})
+				cmp.setup({})
+			end
+		},
+
+		-- LSP
+		{
+			'neovim/nvim-lspconfig',
+			cmd = 'LspInfo',
+			event = { 'BufReadPre', 'BufNewFile' },
+			dependencies = {
+				{ 'hrsh7th/cmp-nvim-lsp' },
+				{ 'williamboman/mason-lspconfig.nvim' },
+				{
+					'williamboman/mason.nvim',
+					build = function()
+						pcall(vim.cmd, 'MasonUpdate')
+					end,
+				},
+			},
+			config = function()
+				-- This is where all the LSP shenanigans will live
+
+				local lsp = require('lsp-zero')
+
+				lsp.on_attach(function(client, bufnr)
+					lsp.default_keymaps({ buffer = bufnr })
+				end)
+
+				-- (Optional) Configure lua language server for neovim
+				require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+				lsp.setup()
+			end
 		}
+
 	})
-
-
---colorscheme
-require('onedark').setup({
-	style = 'dark',
-})
-require('onedark').load()
