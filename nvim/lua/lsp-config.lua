234968b77af4f0473116e68d180fd65ab38af09e
Beicker Zambrano <beicker998@gmail.com>
2023-05-06 20:34:49 -0500

    Move lsp-zero config Lazy config
    Since lsp-zero is now configured properly with lazy loading in the
    plugins.lua file, I don't need it in here

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -1,22 +1,9 @@
---LSP Zero config
-local lsp = require('lsp-zero').preset({})
-
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-end)
-
--- (Optional) Configure lua language server for neovim
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-lsp.setup()
-
 --Format on save
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.ts"}, command = "lua vim.lsp.buf.format({async = false})"})
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.tsx"}, command = "lua vim.lsp.buf.format({async = false})"})
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.js"}, command = "lua vim.lsp.buf.format({async = false})"})
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.jsx"}, command = "lua vim.lsp.buf.format({async = false})"})
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.py"}, command = "lua vim.lsp.buf.format({async = false})"})
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.rs"}, command = "lua vim.lsp.buf.format({async = false})"})
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.astro"}, command = "lua vim.lsp.buf.format({async = false})"})
-vim.api.nvim_create_autocmd("BufWritePre", {pattern = {"*.lua"}, command = "lua vim.lsp.buf.format({async = false})"})
-
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.ts" }, command = "lua vim.lsp.buf.format({async = false})" })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.tsx" }, command = "lua vim.lsp.buf.format({async = false})" })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.js" }, command = "lua vim.lsp.buf.format({async = false})" })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.jsx" }, command = "lua vim.lsp.buf.format({async = false})" })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.py" }, command = "lua vim.lsp.buf.format({async = false})" })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.rs" }, command = "lua vim.lsp.buf.format({async = false})" })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.astro" }, command = "lua vim.lsp.buf.format({async = false})" })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.lua" }, command = "lua vim.lsp.buf.format({async = false})" })
