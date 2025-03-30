7510cfc248658ddcf99e6b3cc1f3571853dade35
Beicker Zambrano <beicker998@gmail.com>
2025-03-29 20:43:42 -0500

    Simplify lsp formatting configs

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -3,18 +3,7 @@
 -- vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.tsx" }, command = "lua vim.lsp.buf.format({async = false})" })
 -- vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.js" }, command = "lua vim.lsp.buf.format({async = false})" })
 -- vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.jsx" }, command = "lua vim.lsp.buf.format({async = false})" })
-vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.py" }, command = "lua vim.lsp.buf.format({async = false})" })
-vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.rs" }, command = "lua vim.lsp.buf.format({async = false})" })
-vim.api.nvim_create_autocmd("BufWritePre",
-	{ pattern = { "*.astro" }, command = "lua vim.lsp.buf.format({async = false})" })
-vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*.lua" }, command = "lua vim.lsp.buf.format({async = false})" })
-vim.api.nvim_create_user_command('LspRename', function()
-	vim.lsp.buf.rename()
-end, {})
-vim.keymap.set("n", "<leader>r", function()
-	vim.cmd([[LspRename]])
-	vim.cmd([[normal! <C-L>]])
-end, { silent = true })
+vim.api.nvim_create_autocmd("BufWritePre", { pattern = { "*" }, command = "lua vim.lsp.buf.format({async = false})" })
 
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
