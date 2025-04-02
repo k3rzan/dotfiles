5df0e97ef9917d6e69a2d2adce9be5b477721fa1
Beicker Zambrano <beicker998@gmail.com>
2025-04-01 20:01:37 -0500

    Replace lspconfig, lsp-zero and nvim-cmp

--- nvim/lua/plugins/installations.lua
+++ nvim/lua/plugins/installations.lua
@@ -259,88 +259,17 @@ require('lazy').setup(
 		--Development
 		{ 'simrat39/rust-tools.nvim', ft = { "rs" } },
 		{
-			'VonHeikemen/lsp-zero.nvim',
-			branch = 'v2.x',
-			lazy = true,
+			dir = "~/.config/nvim/lua/plugins/mylspconfig/",
+			name = "mylspconfig",
 			config = function()
-				-- This is where you modify the settings for lsp-zero
-				-- Note: autocompletion settings will not take effect
-
-				require('lsp-zero.settings').preset({})
-			end
-		},
-
-		-- Autocompletion
-		{
-			'hrsh7th/nvim-cmp',
-			event = 'InsertEnter',
+				require("plugins.mylspconfig").setup()
+			end,
 			dependencies = {
-				{ 'L3MON4D3/LuaSnip' },
-			},
-			config = function()
-				-- Here is where you configure the autocompletion settings.
-				-- The arguments for .extend() have the same shape as `manage_nvim_cmp`:
-				-- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#manage_nvim_cmp
+				require("plugins.blink"),
+				require("plugins.colorful"),
+			}
 
-				require('lsp-zero.cmp').extend()
 
-				-- And you can configure cmp even more, if you want to.
-				local cmp = require('cmp')
-				-- local cmp_action = require('lsp-zero.cmp').action()
-
-				--cmp.setup({
-				--		mapping = {
-				--			['<C-Space>'] = cmp.mapping.complete(),
-				--			['<C-f>'] = cmp_action.luasnip_jump_forward(),
-				--			['<C-b>'] = cmp_action.luasnip_jump_backward(),
-				--		}
-				--	})
-				cmp.setup({})
-			end
 		},
-		-- { 'williamboman/mason-lspconfig.nvim' },
-		-- {
-		-- 	'williamboman/mason.nvim',
-		-- 	build = function()
-		-- 		pcall(vim.cmd, 'MasonUpdate')
-		-- 	end,
-		-- },
-
-		-- LSP
-		{
-			'neovim/nvim-lspconfig',
-			cmd = 'LspInfo',
-			event = { 'BufReadPre', 'BufNewFile' },
-			dependencies = {
-				{ 'hrsh7th/cmp-nvim-lsp' },
-				{ 'hrsh7th/cmp-buffer' },
-			},
-			config = function()
-				-- This is where all the LSP shenanigans will live
-
-				local lspconfig = require('lspconfig')
-				local lsp = require('lsp-zero')
-
-				lsp.on_attach(function(client, bufnr)
-					lsp.default_keymaps({ buffer = bufnr })
-				end)
-
-				-- (Optional) Configure lua language server for neovim
-				lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-				lspconfig.gdscript.setup {
-					flags = {
-						debounce_text_changes = 150,
-					}
-				}
-				-- lspconfig.biome.setup {}
-				lspconfig.nil_ls.setup {}
-				lspconfig.zls.setup {
-					filetypes = { "zig" },
-				}
-				lspconfig.clangd.setup {}
-
-				lsp.setup()
-			end
-		}
 
 	})
