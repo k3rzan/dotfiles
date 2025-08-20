ce04d5090c8bee92bd7c4c8006f4cda88fc3d0af
Kerzan <kerzan.dev@gmail.com>
2025-08-20 07:29:16 -0500

    feat: add config file for blink

--- nvim/lua/plugins/blink/init.lua
+++ nvim/lua/plugins/blink/init.lua
@@ -2,7 +2,16 @@ return {
 	'saghen/blink.cmp',
 
 	version = '1.*',
+	dependencies = {
+		"L3MON4D3/LuaSnip",
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		config = function()
+			require("plugins.snippets")
+		end
+	},
 	opts = {
+		snippets = { preset = "luasnip" },
 		keymap = { preset = 'default' },
 
 		appearance = {
