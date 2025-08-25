1a7c8043e258804202452e9a73512858355e9403
Kerzan <kerzan.dev@gmail.com>
2025-08-25 06:57:58 -0500

    feat: listen to cd OSC 7 and WIP command
    Now I can listen to OSC 7 events to keep my local working directory (per
    window/buffer/tab) synced with the one in my shell. Also I've been
    experimenting with a user command to run arbitrary shell commands and
    show the output in a buffer. It looks kinda like what tsoding does but
    it's not finished yet.

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -69,3 +69,32 @@ vim.cmd("set conceallevel=0")
 -- let g:AutoPairsMapCR=0
 --
 vim.g.skip_ts_context_commentstring_module = true
+
+vim.api.nvim_create_autocmd({ 'TermRequest' }, {
+	desc = 'Handles OSC 7 dir change requests',
+	callback = function(ev)
+		local val, n = string.gsub(ev.data.sequence, '\027]7;file://[^/]*', '')
+		if n > 0 then
+			-- OSC 7: dir-change
+			local dir = val
+			if vim.fn.isdirectory(dir) == 0 then
+				vim.notify('invalid dir: ' .. dir)
+				return
+			end
+			vim.b[ev.buf].osc7_dir = dir
+			if vim.api.nvim_get_current_buf() == ev.buf then
+				vim.cmd.lcd(dir)
+			end
+		end
+	end
+})
+
+vim.api.nvim_create_user_command('TRun',
+	function()
+		local b = vim.api.nvim_create_buf(false, true)
+		vim.api.nvim_open_win(b, true, { win = 0, split = 'below' })
+		vim.cmd.term { "ls -l", bang = true }
+		-- TODO: I can close this automatically when I move the cursor out of the terminal output window
+	end,
+	{}
+)
